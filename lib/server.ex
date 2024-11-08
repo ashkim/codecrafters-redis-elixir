8b2a21120de91eece4a39aba4c9b190fe411aff9
ash kim <ash.kim@gmail.com>
2024-11-08 15:30:44 +1100

    send multi responses

--- lib/server.ex
+++ lib/server.ex
@@ -21,7 +21,33 @@ defmodule Server do
     # Since the tester restarts your program quite often, setting SO_REUSEADDR
     # ensures that we don't run into 'Address already in use' errors
     {:ok, socket} = :gen_tcp.listen(6379, [:binary, active: false, reuseaddr: true])
+
+    loop_acceptor(socket)
+  end
+
+  defp loop_acceptor(socket) do
     {:ok, client} = :gen_tcp.accept(socket)
-    :gen_tcp.send(client, "+PONG\r\n")
+
+    serve(client)
+
+    loop_acceptor(socket)
+  end
+
+  defp serve(socket) do
+    socket
+    |> read_line()
+    |> write_line(socket)
+
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    IO.puts("Command #{data}")
+    data
+  end
+
+  defp write_line(_line, socket) do
+    :gen_tcp.send(socket, "+PONG\r\n")
   end
 end
